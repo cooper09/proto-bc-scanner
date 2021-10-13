7b942916b3035da2ac6cf0b6070bb8159d8acf50
cooper09 <cooperatkmba@gmail.com>
2021-10-13 08:32:58 -0400

    loaded component barcode types

--- App.js
+++ App.js
@@ -52,7 +52,28 @@ export default function App() {
     
       <BarCodeScanner onBarCodeScanned={handleBarCodeScanned}
                             type={type}
-                            barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+                            barCodeTypes={[
+                                BarCodeScanner.Constants.BarCodeType.qr, 
+                                BarCodeScanner.Constants.BarCodeType.aztec,
+                                BarCodeScanner.Constants.BarCodeType.codabar,
+                                BarCodeScanner.Constants.BarCodeType.code39,
+                                BarCodeScanner.Constants.BarCodeType.code93,
+                                BarCodeScanner.Constants.BarCodeType.code128,
+                                BarCodeScanner.Constants.BarCodeType.code39mod43,
+                                BarCodeScanner.Constants.BarCodeType.datamatrix,
+                                BarCodeScanner.Constants.BarCodeType.ean13,
+                                BarCodeScanner.Constants.BarCodeType.ean8,
+                                BarCodeScanner.Constants.BarCodeType.interleaved2of5,
+                                BarCodeScanner.Constants.BarCodeType.itf14,
+                                BarCodeScanner.Constants.BarCodeType.maxicode,
+                                BarCodeScanner.Constants.BarCodeType.pdf417,
+                                BarCodeScanner.Constants.BarCodeType.rss14,
+                                BarCodeScanner.Constants.BarCodeType.rssexpanded,
+                                BarCodeScanner.Constants.BarCodeType.upc_a,
+                                BarCodeScanner.Constants.BarCodeType.upc_e,
+                                BarCodeScanner.Constants.BarCodeType.upc_ean
+                        
+                            ]}
                             style={[StyleSheet.absoluteFillObject, styles.container]}>
                 <View
                     style={{
@@ -76,7 +97,7 @@ export default function App() {
                         <Text style={{fontSize: 18, margin: 5, color: 'white'}}> Flip </Text>
                     </TouchableOpacity>
                 </View>
-                <BarcodeMask edgeColor="#62B1F6" showAnimatedLine width={300} height={150}/>
+                <BarcodeMask edgeColor="#62B1F6" showAnimatedLine width={400} height={200}/>
                 {scanned && <Button title="Scan Again" onPress={() => setScanned(false)}/>}
       </BarCodeScanner>
     </View>
